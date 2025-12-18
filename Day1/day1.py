c9313446e7e856da17591f023dee5070687ff17c
thebamorgan <77521759+thebamorgan@users.noreply.github.com>
2025-12-18 13:09:20 -0600

    Day one complete

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,10 +1,11 @@
 pos = 50
 count = 0
-file_path = "day1ex.txt"  # Replace with the actual path to your file
+numRange = list(range(0,99))
+file_path = "day1.txt"  # Replace with the actual path to your file
 
 try:
     with open(file_path, 'r') as file:
-        for line in file:
+        for line in file:   
             # Each 'line' variable will contain one line from the file,
             # including the newline character ('\n') at the end.
             # You might want to remove it using .strip() or .rstrip().
@@ -15,13 +16,16 @@ try:
                 pos += number
             elif direction == 'L':
                 pos -= number
+            while pos < 0:
+                pos += 100
+            while pos > 99:
+                pos -= 100
             if pos == 0:
                 count += 1
-            if pos < 0:
-                pos += 100
+            
 
 
-            print(count, pos)
+            print(count,direction, number, pos)
 
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
